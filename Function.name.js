9ed9ffa37037a40c5cf1476ca621de4f86874b57
James M. Greene <james.m.greene@gmail.com>
2016-10-22 08:13:01 -0500

    Memoize the resulting name by defining it on the Function instance

--- Function.name.js
+++ Function.name.js
@@ -45,8 +45,24 @@ Function.prototype._name = _name;
 // Polyfill it!
 if (canDefineProp && needsPolyfill) {
   Object.defineProperty(Function.prototype, "name", {
-    get: _name,
-    configurable: true
+    get: function() {
+      var name = _name.call(this);
+
+      // Since named function definitions have immutable names, also memoize the
+      // output by defining the `name` property on this Function instance so
+      // that this polyfill will not need to be invoked again
+      Object.defineProperty(this, "name", {
+        value: name,
+        configurable: true,
+        writable: false,
+        enumerable: false
+      });
+
+      return name;
+    },
+    configurable: true,
+    writable: false,
+    enumerable: false
   });
 }
 
